291d99d1ed738acb04c1f7036484ecfc4366daff
mtunsnef <tuannm23.dev@gmail.com>
2025-06-14 23:10:57 +0700

    feat: login an account

---
+++ RentNest.Infrastructure/Repositories/AccountRepo/AccountRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentNest.Core.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentNest.Infrastructure.Repositories.AccountRepo
+{
+    public class AccountRepository : RepositoryBase<Account>, IAccountRepository
+    {
+        public AccountRepository(Db21027Context context) : base(context) { }
+
+        public async Task<bool> CheckEmailExistsAsync(string email)
+        {
+            return await _dbSet.AnyAsync(a => a.Email == email);
+        }
+
+        public async Task<bool> CheckUsernameExistsAsync(string username)
+        {
+            return await _dbSet.AnyAsync(a => a.Username == username);
+        }
+        public async Task<Account?> GetAccountByEmailAsync(string email)
+        {
+            return await _dbSet.Include(u => u.UserProfile).FirstOrDefaultAsync(a => a.Email == email);
+        }
+
+        public async Task<Account?> GetAccountByEmailOrUsernameAsync(string input)
+        {
+            return await _context.Accounts
+                 .Include(a => a.UserProfile)
+                .FirstOrDefaultAsync(a => a.Email.ToLower() == input.ToLower() || a.Username.ToLower() == input.ToLower());
+        }
+    }
+}
---
+++ RentNest.Service/Services/AccountService/IAccountService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using RentNest.Core.Domains;
+using RentNest.Service.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentNest.Service.Services.AccountService
+{
+    public interface IAccountService
+    {
+        Task<bool> Login(AccountLoginDto accountDto);
+        Task<Account?> GetAccountByEmailAsync(string email);
+        Task Update(Account account);
+        Task<UserProfile?> GetProfileAsync(int accountId);
+        Task UpdateProfileAsync(UserProfile profile);
+        Task<(bool Success, string Message)> UploadAvatarAsync(int accountId, IFormFile avatar, string webRootPath);
+        Task<Account> CreateExternalAccountAsync(ExternalAccountRegisterDto dto);
+        Task<bool> RegisterAccountAsync(AccountRegisterDto model);
+        Task SetUserOnlineAsync(int userId, bool isOnline);
+        Task UpdateLastActiveAsync(int userId);
+        Task<Account?> GetAccountById(int accountId);
+        Task<bool> CheckEmailExistsAsync(string email);
+        Task<bool> CheckUsernameExistsAsync(string username);
+        Task<Account?> GetAccountByEmailOrUsernameAsync(string input);
+    }
+}
