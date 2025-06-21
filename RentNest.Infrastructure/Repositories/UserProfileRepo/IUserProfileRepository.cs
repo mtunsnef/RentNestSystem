e7899321321c2e42b92e4a8576463ef758e0f4b4
mtunsnef <tuannm23.dev@gmail.com>
2025-06-22 01:32:57 +0700

    feat: add func update user profile

---
+++ RentNest.Infrastructure/Repositories/UserProfileRepo/IUserProfileRepository.cs
@@ -0,0 +1,15 @@
+﻿using RentNest.Core.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentNest.Infrastructure.Repositories.UserProfileRepo
+{
+    public interface IUserProfileRepository : IRepositoryBase<UserProfile>
+    {
+        Task<UserProfile?> GetProfileByAccountIdAsync(int accountId);
+        Task<UserProfile?> FindExistingUserProfileAsync(UserProfile updatedProfile);
+    }
+}
