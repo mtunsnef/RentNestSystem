d2e95e328553b85151e855a58ac9af65440d0c6a
mtunsnef <tuannm23.dev@gmail.com>
2025-06-27 15:56:50 +0700

    feat: amenity service

---
+++ RentNest.Infrastructure/Repositories/AmenityRepo/AmenityRepository.cs
@@ -0,0 +1,16 @@
+﻿using RentNest.Core.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentNest.Infrastructure.Repositories.AmenityRepo
+{
+    public class AmenityRepository : RepositoryBase<Amenity>, IAmenityRepository
+    {
+        public AmenityRepository(Db21027Context context) : base(context)
+        {
+        }
+    }
+}
---
+++ RentNest.Service/Services/AmenityService/AmenityService.cs
@@ -0,0 +1,24 @@
+﻿using RentNest.Core.Domains;
+using RentNest.Infrastructure.Repositories.AmenityRepo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentNest.Service.Services.AmenityService
+{
+    public class AmenityService : IAmenityService
+    {
+        private readonly IAmenityRepository _amenitiesRepository;
+
+        public AmenityService(IAmenityRepository amenitiesRepository)
+        {
+            _amenitiesRepository = amenitiesRepository;
+        }
+        public async Task<IEnumerable<Amenity>> GetAll()
+        {
+            return await _amenitiesRepository.GetAllAsync();
+        }
+    }
+}
