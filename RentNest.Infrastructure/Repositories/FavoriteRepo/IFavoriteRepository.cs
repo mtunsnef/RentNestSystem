17ba4993a55d11229b9fcb2261fa7844d61002f9
mtunsnef <tuannm23.dev@gmail.com>
2025-06-27 15:56:12 +0700

    feat: add favorite room

---
+++ RentNest.Infrastructure/Repositories/FavoriteRepo/IFavoriteRepository.cs
@@ -0,0 +1,17 @@
+﻿using RentNest.Core.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentNest.Infrastructure.Repositories.FavoriteRepo
+{
+    public interface IFavoriteRepository : IRepositoryBase<FavoritePost>
+    {
+        Task<bool> IsFavorite(int postId, int accountId);
+        Task RemoveFromFavorite(int postId, int accountId);
+        Task<List<FavoritePost>> GetFavoriteByUser(int accountId);
+
+    }
+}
---
+++ RentNest.Service/Services/FavoriteService/FavoriteService.cs
@@ -0,0 +1,44 @@
+﻿using RentNest.Core.Domains;
+using RentNest.Infrastructure.Repositories.FavoriteRepo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentNest.Service.Services.FavoriteService
+{
+    public class FavoriteService : IFavoriteService
+    {
+        private readonly IFavoriteRepository _favoriteRepo;
+
+        public FavoriteService(IFavoriteRepository favoriteRepo)
+        {
+            _favoriteRepo = favoriteRepo;
+        }
+        public async Task AddToFavorite(int postId, int accountId)
+        {
+            await _favoriteRepo.AddAsync(new FavoritePost
+            {
+                PostId = postId,
+                AccountId = accountId,
+                CreatedAt = DateTime.Now
+            });
+        }
+
+        public async Task<List<FavoritePost>> GetFavoriteByUser(int accountId)
+        {
+            return await _favoriteRepo.GetFavoriteByUser(accountId);
+        }
+
+        public async Task<bool> IsFavorite(int postId, int accountId)
+        {
+            return await _favoriteRepo.IsFavorite(postId, accountId);
+        }
+
+        public async Task RemoveFromFavorite(int postId, int accountId)
+        {
+            await _favoriteRepo.RemoveFromFavorite(postId, accountId);
+        }
+    }
+}
