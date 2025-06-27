2cd466e5678265446914b18e0aaa8639d61cc3c3
mtunsnef <tuannm23.dev@gmail.com>
2025-06-27 15:54:52 +0700

    feat: crud accommodation type service

---
+++ RentNest.Infrastructure/Repositories/AccommodationTypeRepo/AccommodationTypeRepository.cs
@@ -0,0 +1,16 @@
+﻿using RentNest.Core.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentNest.Infrastructure.Repositories.AccommodationTypeRepo
+{
+    public class AccommodationTypeRepository : RepositoryBase<AccommodationType>, IAccommodationTypeRepository
+    {
+        public AccommodationTypeRepository(Db21027Context context) : base(context)
+        {
+        }
+    }
+}
---
+++ RentNest.Service/Services/AccommodationTypeService/AccommodationTypeService.cs
@@ -0,0 +1,24 @@
+﻿using RentNest.Core.Domains;
+using RentNest.Infrastructure.Repositories.AccommodationTypeRepo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentNest.Service.Services.AccommodationTypeService
+{
+    public class AccommodationTypeService : IAccommodationTypeService
+    {
+        private readonly IAccommodationTypeRepository _iAccommodationTypeRepository;
+
+        public AccommodationTypeService(IAccommodationTypeRepository iAccommodationTypeRepository)
+        {
+            _iAccommodationTypeRepository = iAccommodationTypeRepository;
+        }
+        public async Task<IEnumerable<AccommodationType>> GetAllAsync()
+        {
+            return await _iAccommodationTypeRepository.GetAllAsync();
+        }
+    }
+}
