b05e3c05bdf4454445a704983c45ff09d940148d
mtunsnef <tuannm23.dev@gmail.com>
2025-07-06 23:51:13 +0700

    feat: crud post service

---
+++ RentNest.Infrastructure/Repositories/PostCommentRepo/IPostCommentRepository.cs
@@ -0,0 +1,14 @@
+﻿using RentNest.Core.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentNest.Infrastructure.Repositories.PostCommentRepo
+{
+    public interface IPostCommentRepository : IRepositoryBase<PostComment>
+    {
+        Task<List<PostComment>> GetCommentsByPostIdAsync(int postId);
+    }
+}
