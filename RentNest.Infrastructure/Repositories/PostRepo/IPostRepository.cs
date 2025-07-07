27ddccf51f7247c1de15ef54a207e843099b86ff
mtunsnef <tuannm23.dev@gmail.com>
2025-07-07 22:49:09 +0700

    feat: post news for landlord

---
+++ RentNest.Infrastructure/Repositories/PostRepo/IPostRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using RentNest.Core.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentNest.Infrastructure.Repositories.PostRepo
+{
+    public interface IPostRepository : IRepositoryBase<Post>
+    {
+        Task<List<Post>> GetAllPostsWithAccommodation();
+        Task<List<Post>> GetTopVipPostsAsync();
+        Task<Post?> GetPostDetailWithAccommodationDetailAsync(int postId);
+        Task<List<Post>> GetAllPostsByUserAsync(int accountId);
+        Task<IDbContextTransaction> BeginTransactionAsync();
+    }
+}
