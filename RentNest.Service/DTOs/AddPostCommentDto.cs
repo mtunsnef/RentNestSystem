88cd5ac72175f1da997b2c95ec2dd1149ba9ad7f
mtunsnef <tuannm23.dev@gmail.com>
2025-07-16 08:25:35 -0700

    feat: comment/review for post (in user)

---
+++ RentNest.Service/DTOs/AddPostCommentDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentNest.Service.DTOs
+{
+    public class AddPostCommentDto
+    {
+        public int PostId { get; set; }
+        public string Content { get; set; }
+        public int? ParentCommentId { get; set; }
+    }
+}
