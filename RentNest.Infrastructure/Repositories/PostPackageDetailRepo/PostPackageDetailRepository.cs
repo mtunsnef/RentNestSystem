cf99b8fd603494750768720a2961cbf5645e945e
mtunsnef <tuannm23.dev@gmail.com>
2025-07-16 08:24:12 -0700

    feat: post package detail service

---
+++ RentNest.Infrastructure/Repositories/PostPackageDetailRepo/PostPackageDetailRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentNest.Core.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentNest.Infrastructure.Repositories.PostPackageDetailRepo
+{
+    public class PostPackageDetailRepository : RepositoryBase<PostPackageDetail>, IPostPackageDetailRepository
+    {
+        public PostPackageDetailRepository(Db21027Context context) : base(context)
+        {
+        }
+
+        public async Task<PostPackageDetail?> GetByPostIdAsync(int postId)
+        {
+            return await _dbSet
+                .AsNoTracking()
+                .Include(p => p.Pricing)
+                    .ThenInclude(o => o.PackageType)
+                .FirstOrDefaultAsync(p => p.PostId == postId);
+        }
+    }
+}
