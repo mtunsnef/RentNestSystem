3faebfd5838f52804f3492a8383041f5b6573dbe
mtunsnef <tuannm23.dev@gmail.com>
2025-07-17 23:57:14 +0700

    feat: add promo code (voucher) in post

---
+++ RentNest.Service/DTOs/PromotionDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentNest.Service.DTOs
+{
+    public class PromotionDto
+    {
+        public int PromoId { get; set; }
+        public string? Code { get; set; }
+        public string? Description { get; set; }
+        public int? DurationDays { get; set; }
+        public decimal? DiscountAmount { get; set; }
+    }
+
+}
---
+++ RentNest.Service/Services/PromoCodeService/IPromoCodeService.cs
@@ -0,0 +1,14 @@
+﻿using RentNest.Service.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentNest.Service.Services.PromoCodeService
+{
+    public interface IPromoCodeService
+    {
+        Task<List<PromotionDto>> GetPromotionsAsync(string keyword, int accountId);
+    }
+}
