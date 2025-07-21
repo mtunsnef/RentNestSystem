efcf451d416ad6d4d0e30a602a47987356484c01
mtunsnef <tuannm23.dev@gmail.com>
2025-07-22 00:11:18 +0700

    feat: load time unit package

---
+++ RentNest.Infrastructure/Repositories/TimeUnitPackageRepo/ITimeUnitPackageRepository.cs
@@ -0,0 +1,14 @@
+﻿using RentNest.Core.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentNest.Infrastructure.Repositories.TimeUnitPackageRepo
+{
+    public interface ITimeUnitPackageRepository : IRepositoryBase<TimeUnitPackage>
+    {
+        Task<TimeUnitPackage?> GetTimeUnitById(int timeUnitId);
+    }
+}
---
+++ RentNest.Infrastructure/Repositories/TimeUnitPackageRepo/TimeUnitPackageRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentNest.Core.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentNest.Infrastructure.Repositories.TimeUnitPackageRepo
+{
+    public class TimeUnitPackageRepository : RepositoryBase<TimeUnitPackage>, ITimeUnitPackageRepository
+    {
+        public TimeUnitPackageRepository(Db21027Context context) : base(context)
+        {
+        }
+
+        public async Task<TimeUnitPackage?> GetTimeUnitById(int timeUnitId)
+        {
+            return await _dbSet.FirstOrDefaultAsync(t => t.TimeUnitId == timeUnitId);
+        }
+    }
+}
