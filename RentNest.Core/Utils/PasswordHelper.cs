902fdfe3bfdb0e0ec8d4cdd00b8676c69211a5ef
rikai-thattran <that.tran@rikai.technology>
2025-08-28 23:36:14 +0700

    chore: add password helper common

---
+++ RentNest.Core/Utils/PasswordHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentNest.Core.Utils
+{
+    public static class PasswordHelper
+    {
+        public static string HashPassword(string password) => BCrypt.Net.BCrypt.HashPassword(password);
+
+        public static bool VerifyPassword(string password, string hashedPassword) => BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+    }
+}
